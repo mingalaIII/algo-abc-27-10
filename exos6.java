81ec46e490e9ae29ba4240e8d2e3524def2a709a
mingalaIII <mingalaozias.pro@gmail.com>
2025-12-10 12:05:42 +0100

    exos6

--- exos6.java
+++ exos6.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class exos6 {
     public static void main(String[] args) {
-        
+        Scanner clavier = new Scanner (System.in);
+        String prenom;
+        int age;
+        System.out.println ("quel est votre prénom ?");
+        prenom= clavier.nextLine();
+        System.out.println ("quel est votre âge ?");
+        age= clavier.nextInt();
+        System.out.println ("bonjour, je m'appelle "+prenom+" j'ai "+age+" ans");
+        clavier.close();
+
     }
 }
